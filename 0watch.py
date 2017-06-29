7e5f9e194dc575486d60e6ce5c0bfa9f11049b26
Bastian Eicher <bastian@eicher.net>
2017-06-29 21:44:44 +0200

    Initial commit

---
+++ 0watch.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2017, Bastian Eicher
+# See the README file for details, or visit http://0install.net.
+
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import importlib
+import subprocess
+from xml.dom import minidom
+from zeroinstall.injector import namespaces
+
+def die(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+version = '0.1'
+
+parser = argparse.ArgumentParser(description='Scan a website for new releases and trigger 0template if required.')
+parser.add_argument('watch_file', help='Python script that pulls a list of releases from a website')
+parser.add_argument('-o', '--output', help='output directory')
+args = parser.parse_args()
+
+watch_file = os.path.abspath(args.watch_file)
+watch_file_stem = watch_file[:-3]
+template_file = watch_file_stem + '.xml.template'
+feed_file_stem = watch_file_stem if args.output is None else os.path.join(args.output, os.path.basename(watch_file_stem))
+feed_file = feed_file_stem + ".xml"
+def versioned_feed_file(version): return feed_file_stem + '-' + version + '.xml'
+
+if not watch_file.endswith('.py'):
+    die("Watch file must be named *.py, not '{watch_file}'".format(watch_file = watch_file))
+if not os.path.exists(watch_file):
+    die("Watch file '{watch_file}' must exist".format(watch_file = watch_file))
+if not os.path.exists(template_file):
+    die("Template file '{template_file}' must exist".format(template_file = template_file))
+
+sys.path.insert(0, os.path.dirname(watch_file_stem))
+watch_module = importlib.import_module(os.path.basename(watch_file_stem))
+releases = getattr(watch_module, 'releases', None)
+if not releases:
+    die("Watch file must set array of dicts 'releases'")
+
+def already_known(version):
+    if os.path.exists(versioned_feed_file(version)): return True    
+    if os.path.exists(feed_file):
+        doc = minidom.parse(feed_file)
+        for elem in doc.getElementsByTagNameNS(namespaces.XMLNS_IFACE, 'implementation') + doc.getElementsByTagNameNS(namespaces.XMLNS_IFACE, 'group'):
+            v = elem.getAttribute('version')
+            if v == version: return True
+    return False
+
+def flatten(dict):
+    ret = []
+    for (key, value) in dict.items():
+        ret.append(key + '=' + value)
+    return ret
+
+for release in releases:
+    version = release['version']
+    if not already_known(version):
+        subprocess.call(['0template', '--output', versioned_feed_file(version), template_file] + flatten(release))
---
+++ README.md
@@ -0,0 +1,35 @@
+0watch
+======
+
+Copyright Bastian Eicher et al
+
+
+Introduction
+------------
+
+0watch scans websites for new releases using arbitrary Python code snippets.
+When new releases are detected [0template](https://github.com/0install/0template) is used to create/update a [Zero Install](http://0install.net/) feed.
+
+To make the `0watch` command available on your command-line you can run:
+
+    0alias 0watch http://0install.net/tools/0watch.xml
+
+TODO: Explain usage
+
+
+Conditions
+----------
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
