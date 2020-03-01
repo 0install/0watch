e33c849533dec6063392ad455471f1d7506273a5
Bastian Eicher <bastian@eicher.net>
2020-03-01 12:11:20 +0100

    Simplified imports

--- src/0watch.py
+++ src/0watch.py
@@ -4,10 +4,10 @@
 from __future__ import print_function
 
 import argparse
-import os
 import sys
 import imp
 import subprocess
+from os import path
 from xml.dom import minidom
 
 XMLNS_IFACE= 'http://zero-install.sourceforge.net/2004/injector/interface'
@@ -21,18 +21,18 @@ parser.add_argument('watch_file', help='Python script that pulls a list of relea
 parser.add_argument('-o', '--output', help='output directory')
 args = parser.parse_args()
 
-watch_file = os.path.abspath(args.watch_file)
+watch_file = path.abspath(args.watch_file)
 if not watch_file.endswith('.watch.py'):
     die("Watch file must be named *.watch.py, not '{watch_file}'".format(watch_file = watch_file))
-if not os.path.exists(watch_file):
+if not path.exists(watch_file):
     die("Watch file '{watch_file}' must exist".format(watch_file = watch_file))
 watch_file_stem = watch_file[:-9]
 
 template_file = watch_file_stem + '.xml.template'
-if not os.path.exists(template_file):
+if not path.exists(template_file):
     die("Template file '{template_file}' must exist".format(template_file = template_file))
 
-output_stem = watch_file_stem if args.output is None else os.path.join(args.output, os.path.basename(watch_file_stem))
+output_stem = watch_file_stem if args.output is None else path.join(args.output, path.basename(watch_file_stem))
 feed_file = watch_file_stem + '.xml'
 def output_file(version): return output_stem + '-' + version + '.xml'
 
@@ -42,8 +42,8 @@ if not releases:
     die("Watch file must set array of dicts 'releases'")
 
 def already_known(version):
-    if os.path.exists(output_file(version)): return True
-    if os.path.exists(feed_file):
+    if path.exists(output_file(version)): return True
+    if path.exists(feed_file):
         doc = minidom.parse(feed_file)
         for elem in doc.getElementsByTagNameNS(XMLNS_IFACE, 'implementation') + doc.getElementsByTagNameNS(XMLNS_IFACE, 'group'):
             v = elem.getAttribute('version')
